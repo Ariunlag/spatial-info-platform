928b30aa33b7cad910b8e8b142e66ad10a447ed3
Ariunlag <46431233+Ariunlag@users.noreply.github.com>
2026-02-26 23:54:40 -0600

    Initial commit

---
+++ services/core_service/src/test/java/com/smart_museum/core_service/CoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart_museum.core_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
